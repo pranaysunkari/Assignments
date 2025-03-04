c844662e4285ec836d3fef505acd693c791b267c
Pranay Sunkari <pranaysunkari55@outlook.com>
2025-03-04 23:52:39 +0530

    Assignment

---
+++ src/assignments/Assignments1_Arrays.java
@@ -0,0 +1,12 @@
+package assignments;
+
+public class Assignments1_Arrays {
+
+	public static void main(String[] args) {
+		
+		String [] empNames = {"pranay", "Meghana"}:
+			
+		System.out.println(empNames[0]);
+	}
+
+}
